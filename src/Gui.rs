e662f40480265117a3f1d72af5be2ecd8e49af33
Descharges <paul.caillier@utbm.fr>
2021-05-05 17:27:16 +0200

    added gui + static

--- src/Gui.rs
+++ src/Gui.rs
@@ -3,21 +3,74 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
+use sdl2::{Sdl,VideoSubsystem,EventPump};
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::{WindowCanvas, Texture};
-use sdl2::image::{self, LoadTexture, InitFlag};
-use sdl2::rect::Rect;
+use sdl2::render::WindowCanvas;
+use sdl2::rect::{Rect, Point};
 
-struct Gui{
-    context : Sdl,
-    video : VideoSubsystem,
-    canvas : render::Canvas,
+
+pub struct Gui{
+    //context : Sdl,
+    //video : VideoSubsystem,
+    canvas : WindowCanvas,
+    events : EventPump
 }
 
 impl Gui{
-    fn init(&mut self) -> Gui {
-        let self.context = sdl2::init().unwrap();
+    pub fn new() -> Gui{
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+
+
+        let window = video_subsystem.window("Rustboy", 255, 255)
+            .position_centered()
+            .opengl()
+            .build()
+            .expect("could not initialize video subsystem");
+
+        let mut canvas = window.into_canvas().accelerated().build()
+            .expect("could not make a canvas");
+        canvas.set_draw_color((255,255,255));
+        canvas.clear();
+
+        let event_pump = sdl_context.event_pump().unwrap();
+        Gui{
+            //context: sdl_context,
+            //video: video_subsystem,
+            canvas: canvas,
+            events: event_pump
+        }
+    }
+
+    pub fn clear(&mut self){
+        self.canvas.set_draw_color((255,255,255));
+        self.canvas.clear();
+    }
+
+    pub fn update(&mut self) -> bool{
+        for event in self.events.poll_iter(){
+            match event{
+                Event::Quit {..} => {return false;},
+                _ => {}
+            }
+        }
+        self.canvas.present();
+        return true;
+    }
+
+    pub fn pushMatrix(&mut self, mat: &[[u8;255];255]){
+        for i in 0..255{
+            for j in 0..255{
+                match mat[i][j]{
+                    0 => {self.canvas.set_draw_color((255,255,255))},
+                    1 => {self.canvas.set_draw_color((170,170,170))},
+                    2 => {self.canvas.set_draw_color((84,84,84))},
+                    _ => {self.canvas.set_draw_color((0,0,0))},
+                }
+                self.canvas.draw_point(Point::new(i as i32,j as i32));
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,21 @@
 
 mod Hardware;
 mod InstrucArr;
+mod Gui;
+use rand::Rng;
 
-fn main(){}
+fn main(){
+    let mut rng = rand::thread_rng();
+    let mut window: Gui::Gui = Gui::Gui::new();
+    let mut maxiMatrix: [[u8;255];255] = [[0;255];255];
+    while window.update(){
+        for i in 0..255{
+            for j in 0..255{
+                maxiMatrix[i][j] = rng.gen_range(0..4);
+
+            }
+        }
+        window.clear();
+        window.pushMatrix(&maxiMatrix);
+    }
+}
